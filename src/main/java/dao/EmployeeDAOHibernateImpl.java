a40abb563a573c5bde22a1e6749278de84e9ac2a
kwamekert <tomriddle477@gmail.com>
2020-02-22 18:11:31 +0000

    First commit

---
+++ src/main/java/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.List;
+
+import entity.Employee;
+
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
