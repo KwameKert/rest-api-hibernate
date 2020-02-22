a2bd1791fb138b1d140d90ca5e11e235d519dd02
kwamekert <tomriddle477@gmail.com>
2020-02-22 23:24:24 +0000

    Hibernate implementation complete

--- src/main/java/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/dao/EmployeeDAOHibernateImpl.java
@@ -2,14 +2,45 @@ package dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import entity.Employee;
 
+
+@Repository
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 
+	private EntityManager entityManager;
+	
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager entityManager) {
+		super();
+		this.entityManager = entityManager;
+	}
+
+
+
 	@Override
+	@Transactional
 	public List<Employee> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		//get current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		//create query
+		Query<Employee> theQuery = currentSession.createQuery("from Employee", Employee.class);
+		
+		//execute query
+		List<Employee> employees = theQuery.getResultList();
+		
+		return employees;
 	}
 
 }
